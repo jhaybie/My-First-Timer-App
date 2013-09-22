0ee6cb335fda3e4abb1ccd86403cac13d95b9437
jhaybie <jhaybie@Jays-MacBook-Pro.local>
2013-09-22 10:06:51 -0500

    All required features implemented
    Required functionality all working. Still need to update code to get
    sound working.

--- My First Timer App/SecondViewController.m
+++ My First Timer App/SecondViewController.m
@@ -13,6 +13,8 @@
     __weak IBOutlet UILabel *minutesLabel;
     __weak IBOutlet UIButton *startButton;
     __weak IBOutlet UIButton *resetButton;
+    
+    NSTimer *timer;
 }
 
 - (IBAction)startPressed:(id)sender;
@@ -55,11 +57,17 @@ int hours, minutes, seconds, currentTime;
     {
         [startButton setTitle: @"Pause"
                      forState: UIControlStateNormal];
+        timer = [NSTimer scheduledTimerWithTimeInterval: 1.0f
+                                                 target: self
+                                               selector: @selector(updateCounter:)
+                                               userInfo: nil
+                                                repeats: YES];
     }
     else
     {
         [startButton setTitle: @"Resume"
                      forState: UIControlStateNormal];
+        [timer invalidate];
     }
 }
 
@@ -70,6 +78,8 @@ int hours, minutes, seconds, currentTime;
     [startButton setTitle: @"Start"
                  forState:UIControlStateNormal];
     minutesLabel.text = @"00:00:00";
+    [timer invalidate];
+    currentTime = 0;
 }
 
 
